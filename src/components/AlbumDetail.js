8188711fdcb332b28778aac0418f48fdbc417e6a
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2017-05-08 20:40:39 +0900

    Apply style on Card

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,15 +1,76 @@
 import React from 'react';
 import {
   View,
-  Text
+  Text,
+  Image,
 } from 'react-native';
 
-const AlbumDetail = (props) => {
+import Card from './Card';
+import CardSection from './CardSection';
+import Button from './Button';
+
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    thumbnailStyle,
+    headerContentStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle
+  } = styles;
+
   return (
-    <View>
-      <Text>{props.album.title}</Text>
-    </View>
+    <Card>
+      <CardSection>
+        <View style={thumbnailContainerStyle}>
+          <Image
+            style={thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+
+      <CardSection>
+        <Image
+          style={imageStyle}
+          source={{ uri: image }}
+        />
+      </CardSection>
+
+      <CardSection>
+        <Button />
+      </CardSection>
+    </Card>
   );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
 };
 
 export default AlbumDetail;
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {
-  View
+  ScrollView
 } from 'react-native';
 
 import AlbumDetail from './AlbumDetail';
@@ -24,9 +24,9 @@ class AlbumList extends Component {
     console.log(this.state);
 
     return (
-      <View>
+      <ScrollView>
         {this.renderAlbums()}
-      </View>
+      </ScrollView>
     );
   }
 }
