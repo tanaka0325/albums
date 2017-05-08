fb92421ae11d01e580330bb89d369e9057943e34
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2017-05-08 15:24:48 +0900

    Add AlbumList component

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import {
+  View,
   AppRegistry
 } from 'react-native';
 
 import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 const App = () => (
-  <Header headerText={'Albums'} />
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 export default App;
